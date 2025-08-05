e5b0c3849e5dda78455c54b1f3372f42e55020c1
Arthur Axiotes <axiotesarthur@gmail.com>
2025-08-05 18:47:36 -0300

    chore: configuracao do banco de dados utilizando .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServicesModule } from './common/services/services.module';
 
 @Module({
-  imports: [ServicesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'),
+      }),
+    }),
+
+    ServicesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
