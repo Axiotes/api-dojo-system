a1feb088745ae834ff48b24b0feca669ae1289df
Arthur Axiotes <axiotesarthur@gmail.com>
2025-08-07 16:33:10 -0300

    feat: schema para historico de aulas

--- src/modules/classes/classes.module.ts
+++ src/modules/classes/classes.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { ClassesSchema } from './schemas/classes.schema';
+import { ClassesHistorySchema } from './schemas/classes-history.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'Classes', schema: ClassesSchema }]),
+    MongooseModule.forFeature([
+      { name: 'Classes', schema: ClassesSchema },
+      { name: 'ClassesHistory', schema: ClassesHistorySchema },
+    ]),
   ],
 })
 export class ClassesModule {}
