7c77c84e5f3a60e387d3c28a4e7939799e7a6510
Arthur Axiotes <axiotesarthur@gmail.com>
2025-08-11 23:05:56 -0300

    feat: funcao para criar admin no service de admin

--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -1,14 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import { AdminDto } from './dtos/admin.dto';
 import { Admin } from './schemas/admin.schema';
 
-
 @Injectable()
 export class AdminService {
   constructor(@InjectModel(Admin.name) private adminModel: Model<Admin>) {}
 
-  public async createAdmin(adminDto: AdminDto): Promise<AdminDto> {}
+  public async createAdmin(adminDto: AdminDto): Promise<AdminDto> {
+    const admin = await this.adminModel
+      .findOne({ email: adminDto.email })
+      .exec();
+
+    if (admin) {
+      throw new ConflictException('An admin with this email already exists');
+    }
+
+    return await this.adminModel.create(adminDto);
+  }
 }
