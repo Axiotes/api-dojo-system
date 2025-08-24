e8f3df95b4123f973065d757a298a5f1a6b5af02
Arthur Axiotes <axiotesarthur@gmail.com>
2025-08-24 19:20:36 -0300

    feat: documentacao do endpoint no swagger e limite de 10 reqs por minuto

--- src/modules/plans/dtos/plan.dto.ts
+++ src/modules/plans/dtos/plan.dto.ts
@@ -1,17 +1,34 @@
 import { IsEnum, IsMongoId, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 import { Period } from '@ds-enums/period.enum';
 
 export class PlanDto {
+  @ApiProperty({
+    description: 'Unidade do período do plano, enum: ("monthly", "annually")',
+    example: 'monthly',
+  })
   @IsEnum(['monthly', 'annually'])
   period: Period;
 
+  @ApiProperty({
+    description: 'Quantidade do período definido em period',
+    example: '3',
+  })
   @IsNumber()
   periodQuantity: number;
 
+  @ApiProperty({
+    description: 'Preço do plano',
+    example: '150.00',
+  })
   @IsNumber()
   value: number;
 
+  @ApiProperty({
+    description: 'Referência para a modalidade à qual o plano pertence',
+    example: '64f1b2a3c4d5e6f7890abc12',
+  })
   @IsMongoId()
   modality: string;
 }
