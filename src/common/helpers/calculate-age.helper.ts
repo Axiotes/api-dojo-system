d741342721395e0f623763ce4854b3dfbb8e9058
Arthur Axiotes <axiotesarthur@gmail.com>
2025-10-15 09:56:01 -0300

    fix: calculo de idade apenas pelo ano

--- src/common/helpers/calculate-age.helper.ts
+++ src/common/helpers/calculate-age.helper.ts
@@ -1,10 +1,5 @@
 export const calculateAge = (birthDate: Date): number => {
   const today = new Date();
-  let age = today.getFullYear() - birthDate.getFullYear();
 
-  if (today.getMonth() < birthDate.getMonth()) {
-    age--;
-  }
-
-  return age;
+  return today.getFullYear() - birthDate.getFullYear();
 };
