3ec73fdb948db373051e0420765eb42667603c97
Arthur Axiotes <axiotesarthur@gmail.com>
2025-10-19 21:04:02 -0300

    fix: ajuste no calculo de idade do atleta

--- src/common/helpers/calculate-age.helper.ts
+++ src/common/helpers/calculate-age.helper.ts
@@ -1,5 +1,14 @@
 export const calculateAge = (dateBirth: Date): number => {
   const today = new Date();
+  const monthDiff = today.getMonth() - dateBirth.getMonth();
+  let age = today.getFullYear() - dateBirth.getFullYear();
 
-  return today.getFullYear() - dateBirth.getFullYear();
+  if (
+    monthDiff < 0 ||
+    (monthDiff === 0 && today.getDate() < dateBirth.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
 };
